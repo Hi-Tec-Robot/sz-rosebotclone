5e42445f2dfb7af7869e896e738fc81932a1ebfb
ThilinaWeerasekara <84437905+Thilinaweerasekara2003@users.noreply.github.com>
2022-07-07 22:21:44 +0530

    Update

--- Rose/plugins/chatbot.py
+++ Rose/plugins/chatbot.py
@@ -68,21 +68,35 @@ async def szcbot(_, message: Message):
     chat = chatb.find_one({"chatbot":chat_id})   
     if chat:
        await app.send_chat_action(message.chat.id, "typing")
-       lang = tr.translate(message.text).src
-       trtoen = (message.text if lang=="en" else tr.translate(message.text, dest="en").text).replace(" ", "%20")
-       text = trtoen.replace(" ", "%20") if len(message.text) < 2 else trtoen
-       affiliateplus = requests.get(f"https://api.affiliateplus.xyz/api/chatbot?message={text}&botname=Rose&ownername=@supunma&user=1")
-       textmsg = (affiliateplus.json()["message"])
-       if "Affiliate+" in textmsg:
-        textmsg = textmsg.replace("Affiliate+", "Rose bot created by @szteambots")
-       if "Lebyy_Dev" in textmsg:
-        textmsg = textmsg.replace("Lebyy_Dev", "Supun Maduranga Owner of @szteambots")
-       if "God Brando" in textmsg:
-        textmsg = textmsg.replace("God Brando", f"{message.from_user.first_name}")
-       if "seeker" in textmsg:
-        textmsg = textmsg.replace("seeker", f"wow")
-       msg = tr.translate(textmsg, src='en', dest=lang)
-       await message.reply_text(msg.text)
+        try:
+           lang = tr.translate(message.text).src
+           trtoen = (message.text if lang=="en" else tr.translate(message.text, dest="en").text).replace(" ", "%20")
+           text = trtoen.replace(" ", "%20") if len(message.text) < 2 else trtoen
+           affiliateplus = requests.get(f"https://api.affiliateplus.xyz/api/chatbot?message={text}&botname=Isabella&ownername=@DarkRider2003&user={chat_id}")
+           textmsg = (affiliateplus.json()["message"])
+           if "Affiliate+" in textmsg:
+               textmsg = textmsg.replace("Affiliate+", "Isabella")
+           if "Lebyy_Dev" in textmsg:
+               textmsg = textmsg.replace("Lebyy_Dev", "DarkRider")
+           if "God Brando" in textmsg:
+               textmsg = textmsg.replace("God Brando", f"{message.from_user.first_name}")
+           if "seeker" in textmsg:
+               textmsg = textmsg.replace("seeker", f"wow")
+           msg = tr.translate(textmsg, src='en', dest=lang)
+           await message.reply_text(msg.text)
+        except Exception:
+           user_id = message.from_user.id
+           lang = tr.translate(message.text).src
+           trtoen = (message.text if lang=="en" else tr.translate(message.text, dest="en").text).replace(" ", "%20")
+           text = trtoen.replace(" ", "%20") if len(message.text) < 2 else trtoen
+           safeone = requests.get(f"https://api.safone.tech/chatbot?message={text}&bot_name=Isabella&bot_master=DarkRider&user_id={user_id}")
+           textmsg = (safeone.json()["answer"])
+           if "Affiliate+" in textmsg:
+               textmsg = textmsg.replace("Affiliate+", "Isabella")
+           if "[Safone]" in textmsg:
+               textmsg = textmsg.replace("[Safone]", "Thilina Weerasekara")
+           msg = tr.translate(textmsg, src='en', dest=lang)
+           await message.reply_text(msg.text)
 
 
 
