e183e0f9c6604b7bf8c1cbd4060393545cac8093
ThilinaWeerasekara <84437905+Thilinaweerasekara2003@users.noreply.github.com>
2022-07-08 12:43:37 +0530

    Update tagallert.py

--- Rose/plugins/tagallert.py
+++ Rose/plugins/tagallert.py
@@ -1,4 +1,4 @@
-from Rose import app
+   from Rose import app
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from Rose.utils.commands import *
@@ -9,28 +9,26 @@ from button import *
 def get_info(id):
     return taggeddb.find_one({"id": id})
 
-
 @app.on_message(command(["tagalert"]))
 @language
 async def locks_dfunc(client, message: Message, _):
    lol = await message.reply(_["spoil2"])
    if len(message.command) != 2:
       return await lol.edit(_["tagg1"])
+
    parameter = message.text.strip().split(None, 1)[1].lower()
-  
+
    if parameter == "on" or parameter=="ON":
      if not message.from_user:
        return
      if not message.from_user.username:
        return await lol.edit(_["tagg2"])
+
      uname=str(message.from_user.username)
      uname = uname.lower()
-     isittrue = taggeddb.find_one({f"teg": uname})
-     if not isittrue:
-          taggeddb.insert_one({f"teg": uname})
-          return await lol.edit(_["tagg3"].format(uname))
-     else:
-          return await lol.edit(_["tagg4"])
+     taggeddb.insert_one({f"teg": uname})
+     return await lol.edit(_["tagg3"].format(uname))
+
    if parameter == "off" or parameter=="OFF":
      if not message.from_user:
        return
@@ -38,12 +36,8 @@ async def locks_dfunc(client, message: Message, _):
        return await lol.edit(_["tagg2"])
      uname = message.from_user.username
      uname = uname.lower()
-     isittrue = taggeddb.find_one({f"teg": uname})
-     if isittrue:
-          taggeddb.delete_one({f"teg": uname})
-          return await lol.edit(_["tagg5"])
-     else:
-          return await lol.edit(_["tagg6"])
+     taggeddb.delete_one({f"teg": uname})
+     return await lol.edit(_["tagg5"])
    else:
      await lol.edit(_["tagg1"])
        
@@ -65,7 +59,7 @@ async def mentioned_alert(client, message):
             text = Rose.split()[0]
         isittrue = taggeddb.find_one({f"teg": text})    
         if isittrue == None:
-          return   
+          return
         if text == message.chat:
           return 
         try:
@@ -74,7 +68,6 @@ async def mentioned_alert(client, message):
         except:
             return message.continue_propagation()
         user_ = message.from_user.mention or f"@{message.from_user.username}"
-        
         final_tagged_msg = f"""
 **🗣 You Have Been Tagged**
 **Group:** {chat_name}
@@ -82,12 +75,10 @@ async def mentioned_alert(client, message):
 **Message:**
 {message.text} """
         button_s = InlineKeyboardMarkup([[InlineKeyboardButton("📮 View Message", url=tagged_msg_link)]])
-        """
         try:
             sz = await client.send_message(chat_id=f"{text}", text=final_tagged_msg,reply_markup=button_s,disable_web_page_preview=True)
             pin = await sz.pin(disable_notification=True, both_sides=True)
             await pin.delete()
-            
         except:
             return message.continue_propagation()
         message.continue_propagation()
@@ -98,16 +89,13 @@ __MODULE__ = f"{Tagalert}"
 __HELP__ = """
 Too many mentions.. Cant you manage them all alone..
 Here is the solution
-
 If you are tagged/mentioned in a group where Rose is present
 Rose will notify it to you via private message after enabling tag alerts
-
 **Commands**
 - /tagalert `on` : Turn tag alerts on
 - /tagalert `off` : Turn tag alert off
-
 **Example:**
 If you are mentioned in a group Rose will tell you who mentioned you, 
 message that you are tagged in and which group is that.
 """
-    
+     
