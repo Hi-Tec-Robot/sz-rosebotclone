dc86c0f228ea3776ea2ee9fdfc3f5ebd92b0d935
ThilinaWeerasekara <84437905+Thilinaweerasekara2003@users.noreply.github.com>
2022-07-25 22:41:16 +0530

    Update welcome.py

--- Rose/Inline/welcome.py
+++ Rose/Inline/welcome.py
@@ -4,9 +4,7 @@ from pyrogram.types import InlineKeyboardButton,InlineKeyboardMarkup
 
 texts = """
 Here is the help for Welcome:
-
 Give your members a warm welcome with the greetings module! Or a sad goodbye... Depends!
-
 Admin commands:
 - /welcome : Enable/disable welcomes messages.
 - /goodbye : Enable/disable goodbye messages.
@@ -16,7 +14,6 @@ Admin commands:
 - /resetgoodbye: Reset the goodbye message.
 - /cleanservice : Delete all service messages. Those are the annoying 'x joined the group' notifications you see when people join.
 - /cleanwelcome : Delete old welcome messages. When a new person joins, or after 5 minutes, the previous message will get deleted.
-
 Examples:
 - Get the welcome message without any formatting
 -> /welcome noformat
@@ -32,32 +29,27 @@ fbuttonss = InlineKeyboardMarkup(
 
 @app.on_callback_query(filters.regex("_mdowns"))
 async def commands_callbacc(_, CallbackQuery):
-    await app.send_message(
-        CallbackQuery.message.chat.id,
+    await CallbackQuery.message.edit(
         text=texts,
         reply_markup=fbuttonss,
         disable_web_page_preview=True,
         parse_mode="html"
     )
-    await CallbackQuery.message.delete()
 
 tetz = """
 Rose which will ask new Group Members to verify them by solving an emoji | number captcha.
-
 - /captcha - turn on captcha : There are two types of captcha
 - /remove - turn off captcha
-
 for more help ask in my support group
 """
 @app.on_callback_query(filters.regex("_filling"))
 async def commands_callbacc(_, CallbackQuery):
-    await app.send_message(
-        CallbackQuery.message.chat.id,
+    await CallbackQuery.message.edit(
         text=tetz,
         reply_markup=close,
         disable_web_page_preview=True,
     )
-    await CallbackQuery.message.delete()
+
 
 close = InlineKeyboardMarkup(
         [[
