2e2cb3e94d2dade50c2f1af130756cafbb18a830
ThilinaWeerasekara <84437905+Thilinaweerasekara2003@users.noreply.github.com>
2022-09-07 14:54:57 +0530

    Update fsub.py

--- Rose/plugins/fsub.py
+++ Rose/plugins/fsub.py
@@ -2,9 +2,9 @@
 
 import asyncio
 from pyrogram import Client
-from pyrogram.errors import FloodWait, UserNotParticipant
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from config import Config
+Import name from config
 
 CHANNEL_ID = Config.F_SUB_CHANNEL
 
