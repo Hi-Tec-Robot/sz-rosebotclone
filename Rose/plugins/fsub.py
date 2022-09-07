b8922daa870e8fa9ad73456507d4e0c63bca365b
ThilinaWeerasekara <84437905+Thilinaweerasekara2003@users.noreply.github.com>
2022-09-08 00:35:08 +0530

    Update fsub.py

--- Rose/plugins/fsub.py
+++ Rose/plugins/fsub.py
@@ -2,9 +2,9 @@
 
 import asyncio
 from pyrogram import Client
+from pyrogram.errors import FloodWait, UserNotParticipant
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from config import Config
-Import name from config
 
 CHANNEL_ID = Config.F_SUB_CHANNEL
 
@@ -17,7 +17,7 @@ async def ForceSub(bot: Client, event: Message):
            gh = await bot.send_message(chat_id=event.chat.id,text=f"""
 <b>Hey </b>{event.from_user.mention} !,
 <b>You are Free user so join my creators channel before useing me !Click join now button and join sz channel.</b>
-<i>Don't forget to give</i><code>/start</code><i>command again.</i>""",reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Join Now ↗️", url="https://t.me/szroseupdates")]]),disable_web_page_preview=True)
+<i>Don't forget to give</i><code>/start</code><i>command again.</i>""",reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Join Now ↗️", url="https://t.me/szteambots")]]),disable_web_page_preview=True)
            await asyncio.sleep(10)
            await gh.delete()
            return 400
